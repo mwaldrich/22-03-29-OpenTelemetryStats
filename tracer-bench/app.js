d96bcacb365674063025c2193f9df6734bbd9c94
Mark Aldrich <mark@mwaldrich.io>
2022-04-12 10:08:35 -0400

    forgot to commit: warm VM & gather stats

--- tracer-bench/app.js
+++ tracer-bench/app.js
@@ -3,19 +3,39 @@
 const { tracer } = require('./tracing')
 const { rdtsc } = require('rdtsc')
 
-for (let i = 0; i < 10; i += 1) {
-    doWork();
 
-    for (let i = 0; i < 1_000_000; i++) {
-        /* no-op */
+// warm()
+bench()
+
+function warm() {
+    for (let i = 0; i < 1000; i += 1) {
+        doWork();
+    }
+}
+
+function bench() {
+    for (let i = 0; i < 1000; i++) {
+        const {cycles, ms} = doWork()
+
+        console.error(`${cycles},${ms}`)
     }
 }
 
 function doWork() {
-    const before = rdtsc()
+    const before_time = performance.now()
+    const before_cycles = rdtsc()
     const parentSpan = tracer.startSpan('main')
     parentSpan.end()
-    const after = rdtsc()
+    const after_cycles = rdtsc()
+    const after_time = performance.now()
 
-    console.log(`CPU Cycles: ${after-before}`)
+    console.log(`rdtsc overhead: ${rdtsc() - rdtsc()}`)
+    console.log(`wall time overhead: ${performance.now() - performance.now()}`)
+
+    const elapsed_cycles = after_cycles-before_cycles
+    const elapsed_time = after_time-before_time
+    console.log(`CPU Cycles: ${elapsed_cycles}`)
+    console.log(`Wall time: ${elapsed_time}ms`)
+
+    return {cycles: elapsed_cycles, ms: elapsed_time}
 }
