0000d4eb09c36bce2bfa2e83a4659921adf5bf08
Mark Aldrich <mark@mwaldrich.io>
2022-04-23 12:29:00 -0400

    configurable

--- tracer-bench/tracing.js
+++ tracer-bench/tracing.js
@@ -1,10 +1,39 @@
-const { BasicTracerProvider, ConsoleSpanExporter, SimpleSpanProcessor } = require('@opentelemetry/sdk-trace-base');
+const { BasicTracerProvider, ConsoleSpanExporter, SimpleSpanProcessor, BatchSpanProcessor } = require('@opentelemetry/sdk-trace-base');
 const opentelemetry = require('@opentelemetry/api');
+const fs = require('fs')
+
+// Read in environment variables for configuration
+const BATCH = process.env["BATCH"] == 1
+const  DISK = process.env["DISK"]  == 1
 
 const provider = new BasicTracerProvider();
 
-// Configure span processor to send spans to the exporter
-provider.addSpanProcessor(new SimpleSpanProcessor(new ConsoleSpanExporter()));
+// Configure span processor to send spans to the exporter.
+// This is configured via environment variables.
+let spanExporter;
+if (!DISK) {
+    console.log(`Using STDOUT, DISK=${process.env["DISK"]}`)
+    spanExporter = new ConsoleSpanExporter();
+} else {
+    console.log(`Using file, DISK=${process.env["DISK"]}`)
+    spanExporter = {
+        export: (spans) => {
+            console.error(`Exporting ${spans.length} span s...`)
+            fs.appendFileSync("./output.txt", spans.map(JSON.stringify).join("\n"))
+        }
+    };
+}
+
+let spanProcessor;
+if (BATCH) {
+    spanProcessor = new BatchSpanProcessor(spanExporter)
+} else {
+    spanProcessor = new SimpleSpanProcessor(spanExporter)
+}
+
+
+
+provider.addSpanProcessor(spanProcessor);
 provider.register();
 
 // This is what we'll access in all instrumentation code
