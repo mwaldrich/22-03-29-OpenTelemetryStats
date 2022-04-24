a80f2ed4196f3ec4e48a3f3e3a18eb591639fb4e
Mark Aldrich <mark@mwaldrich.io>
2022-04-24 11:58:36 -0400

    expeeriment v4

--- tracer-bench/run-experiment.sh
+++ tracer-bench/run-experiment.sh
@@ -5,6 +5,4 @@ rm -r node_modules
 npm install
 
 # Run experiment
-env     DATATYPE=STRING1 node app.js 2> js_string1.csv
-env  DATATYPE=STRING1000 node app.js 2> js_string1000.csv
-env DATATYPE=STRING10000 node app.js 2> js_string10000.csv
+node app.js 2> js_string1.csv
